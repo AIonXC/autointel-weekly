46661967855769c325731c3c4db1e217a023346e
AIonXC <aionxc@duck.com>
2025-09-22 17:52:22 -0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>¡Hola, IntelAuto! 🚗</h1>
+      <p>Tu sitio está funcionando correctamente.</p>
+    </div>
+  );
+}
